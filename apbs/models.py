de0d660166927a157063a8e9b9fb732d46d25a2d
Tim Miller <btmiller@helix.nih.gov>
2013-06-26 03:30:29 +0000

    New APBS code from Scott
    git-svn-id: https://charmming.googlecode.com/svn/trunk/head@522 22cb880c-9e46-0410-a028-d5b2de49818f

--- apbs/models.py
+++ apbs/models.py
@@ -19,21 +19,84 @@ from django.db import models
 from django.contrib.auth.models import User
 from structure.models import Task, WorkingFile
 
+import os
+
+
 class apbsParams(models.Model):
-    gdim_x = models.PositiveIntegerField(default=0)
-    gdim_y = models.PositiveIntegerField(default=0)
-    gdim_z = models.PositiveIntegerField(default=0)
-    npoint_x = models.PositiveIntegerField(default=0)
-    npoint_y = models.PositiveIntegerField(default=0)
-    npoint_z = models.PositiveIntegerField(default=0)
+    gdim_x    = models.PositiveIntegerField(default=0)
+    gdim_y    = models.PositiveIntegerField(default=0)
+    gdim_z    = models.PositiveIntegerField(default=0)
+    npoint_x  = models.PositiveIntegerField(default=0)
+    npoint_y  = models.PositiveIntegerField(default=0)
+    npoint_z  = models.PositiveIntegerField(default=0)
 
 class redoxTask(Task):
-    redoxsite = models.CharField(max_length=250) # segid + 1-/2- or 2-/3-
+    redoxsite  = models.CharField(max_length=250) # segid + 1-/2- or 2-/3-
     apbsparams = models.ForeignKey(apbsParams,null=True)
+    #couple = models.CharField(max_length=250)
+    #chargeko = models.CharField(max_length=250)
 
     def finish(self):
         # This is a pain, since we need to get all of the input, output, and
         # PDB files from the REDOX calculation.
+        """test if the job suceeded, create entries for output"""
+        
+        loc = self.workstruct.structure.location
+        bnm = self.workstruct.identifier
+
+        # There's always an input file, so create a WorkingFile
+        # for it.
+        filenamesuffix=['-build-redall',
+                        '-build-redsite',
+                        '-build-oxiall',
+                        '-build-oxisite',
+                        '-combinegrid-site',
+                        '-mkgrid-full',
+                        '-mkgrid-site',
+                        '-modpot',
+                        '-modpotref',
+                        '-oxipot',
+                        '-oxipotref']
+
+        for suffix in filenamesuffix:
+            wfinp = WorkingFile()
+            wfinp.task = self
+            wfinp.path = loc + '/redox-' + bnm + suffix + '.inp'
+            wfinp.canonPath = wfinp.path
+            wfinp.type = 'inp'
+            wfinp.description = 'a redox input script'
+            wfinp.save()
+
+        # Check if an output file was created and if so create
+        # a WorkingFile for it.
+        logfp = open('/tmp/redoxdone.txt', 'w')
+        for suffix in filenamesuffix:
+            logfp.write('looking for %s\n' % (loc + '/redox-' + bnm + suffix + '.out'))
+            try:
+                os.stat(loc + '/redox-' + bnm + suffix + '.out')
+                logfp.write('OK!\n')
+            except:
+                logfp.write('Bad\n')
+                self.status = 'C'
+                self.finished = 'y'
+                return
+            wfout = WorkingFile()
+            wfout.task = self
+            wfout.path = loc + '/redox-' + bnm + suffix + '.out'
+            wfout.canonPath = wfout.path
+            wfout.type = 'out'
+            wfout.description = 'a redox script output'
+            wfout.save()
+        self.status = 'C'
+        logfp.close()
+
+        if self.status == 'F':
+            return
+
+        # create Working files for PDB, CRD, and PSF.
+        '''
+        OH WOW! I just stopped caring!!!!
+        '''
 
         self.status = 'C'
-        self.finished = 'y'
+        #self.finished = 'y'
